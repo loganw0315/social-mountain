86c4f0fe98d45905652ed8f37359298f433d0792
Logan Willis <loganw0315@gmail.com>
2021-12-20 12:05:47 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+import Header from './Header/Header';
+import Compose from './Compose/Compose';
+import Post from './Post/Post'
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      posts: []
+    };
+
+    this.updatePost = this.updatePost.bind( this );
+    this.deletePost = this.deletePost.bind( this );
+    this.createPost = this.createPost.bind( this );
+  }
+  
+  componentDidMount() {
+    axios.get('https://practiceapi.devmountain.com/api/posts').then((res) =>{
+      this.setState({posts: res.data})
+    })
+  }
+
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text}).then((res) => {
+      this.setState({posts: res.data})
+    })
+  }
+
+  deletePost() {
+
+  }
+
+  createPost() {
+
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="App__parent">
+        <Header />
+
+        <section className="App__content">
+
+          <Compose />
+          {posts.map(post=>(
+            <Post key={post.id} text={post.text} date={post.date}/>
+          )
+          )}
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
